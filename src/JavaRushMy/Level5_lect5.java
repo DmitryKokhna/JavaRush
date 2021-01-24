9616d223ebe694fb304d177f7ec8e6e7a6f96408
Dmitry Kokhna <dmitry.kokhna@gmail.com>
2021-01-24 17:00:01 +0300

    final

--- src/JavaRushMy/Level5_lect5.java
+++ src/JavaRushMy/Level5_lect5.java
@@ -1,10 +1,16 @@
 package JavaRushMy;
 
+//Чётные и нечётные циферки
+//        Давайте определим, сколько четных цифр, а сколько нечетных в числе, введенном с клавиатуры.
+//        Если число делится без остатка на 2 (т. е. остаток равен нулю), значит оно четное.
+//        Увеличиваем на 1 счетчик четных цифр (статическая переменная even).
+//        Иначе число нечетное, увеличиваем счетчик нечетных цифр (статическая переменная odd).
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Main {
+public class Level5_lect5 {
     public static int even;
     public static int odd;
 

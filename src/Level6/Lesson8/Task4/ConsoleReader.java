95cc47348daa2f8427764aeb107a8ce01f6d7f80
Dmitry Kokhna <dmitry.kokhna@gmail.com>
2021-01-26 23:13:33 +0300

    add Task 4 Lesson 8 Level 6

---
+++ src/Level6/Lesson8/Task4/ConsoleReader.java
@@ -0,0 +1,39 @@
+package Level6.Lesson8.Task4;
+// ЗАДАЧА 4
+//Класс ConsoleReader
+// MEDIUM
+
+//Сегодня студенты секретного центра JavaRush получили задание по чтению.
+//Им (и вам) нужно создать класс ConsoleReader с 4 статическими методами: один из них считывает с клавиатуры строку, другой — целое число, третий — дробное.
+//Четвертый отличается: он считывает с клавиатуры строку true или false и возвращает соответствующую логическую переменную.
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class ConsoleReader {
+    public static String readString() throws IOException {
+        BufferedReader reader=new BufferedReader(new InputStreamReader(System.in));
+        String str=reader.readLine();
+        return  str;
+            }
+
+            public  static int readInt() throws IOException{
+                BufferedReader reader=new BufferedReader(new InputStreamReader(System.in));
+                int in=Integer.parseInt(reader.readLine());
+                return in;
+            }
+
+            public  static double readDouble() throws IOException{
+                BufferedReader reader=new BufferedReader(new InputStreamReader(System.in));
+        double  db=Double.parseDouble(reader.readLine());
+        return  db;
+            }
+
+            public static boolean readBoolean () throws IOException{
+                BufferedReader reader=new BufferedReader(new InputStreamReader(System.in));
+                boolean bl=Boolean.parseBoolean(reader.readLine());
+                return bl;
+            }
+}

6093413c7f64863b5cc1895fb97526a0c9299eb3
Dmitry Kokhna <dmitry.kokhna@gmail.com>
2021-01-31 23:24:36 +0300

    first add Task 7 Lesson 8 Level 6 // second_br + add text

--- src/Level6/Lesson8/Task7/Calcutator.java
+++ src/Level6/Lesson8/Task7/Calcutator.java
@@ -40,7 +40,7 @@ public class Calcutator {
         System.out.println("Умноженее чисел равно: " +multyply(8,8));
         System.out.println("Деление чисел равно: " +devision(70,2));
         System.out.println("Процент от чисела А составляет: " +percent(70,50));
-        // second_br
+
     }
 
 }

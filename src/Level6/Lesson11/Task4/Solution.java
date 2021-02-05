c17708a3adb9c49b39c0b94e395c2c51671ea1f9
Dmitry Kokhna <dmitry.kokhna@gmail.com>
2021-02-05 21:39:45 +0300

    add Task 4 Lesson 11 Level 6

---
+++ src/Level6/Lesson11/Task4/Solution.java
@@ -0,0 +1,31 @@
+package Level6.Lesson11.Task4;
+// ЗАДАЧА 4
+// Минимальное число статиков
+// Уровень - Medium
+//Расставьте минимальное количество static-ов, чтобы код начал работать, и программа успешно завершилась.
+public class Solution {
+    public int step;
+
+    public static void main(String[] args) {
+        method1();
+    }
+    public void  method1() {
+        method2();
+            }
+            public void method2() {
+        new Solution().method3();
+            }
+            public void method3() {
+        method4();
+            }
+            public void method4()
+            {
+                step++;
+                for (StackTraceElement element :  Thread.currentThread().getStackTrace())
+                    System.out.println(element);
+                if(step>1)
+                    return;
+                main(null);
+
+            }
+}

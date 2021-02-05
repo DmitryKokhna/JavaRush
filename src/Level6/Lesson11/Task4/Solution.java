b68c3e6a467f372e73279551e8c813ec5788242e
Dmitry Kokhna <dmitry.kokhna@gmail.com>
2021-02-05 22:05:09 +0300

    final Level6.Lesson11.Task4

--- src/Level6/Lesson11/Task4/Solution.java
+++ src/Level6/Lesson11/Task4/Solution.java
@@ -4,15 +4,15 @@ package Level6.Lesson11.Task4;
 // Уровень - Medium
 //Расставьте минимальное количество static-ов, чтобы код начал работать, и программа успешно завершилась.
 public class Solution {
-    public int step;
+    public static int step;
 
     public static void main(String[] args) {
         method1();
     }
-    public void  method1() {
+  static   public void  method1() {
         method2();
             }
-            public void method2() {
+          static   public void method2() {
         new Solution().method3();
             }
             public void method3() {

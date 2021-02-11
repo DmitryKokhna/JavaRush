960ff109aa5ba30c2861fae6ecbce3699011a68e
Dmitry Kokhna <dmitry.kokhna@gmail.com>
2021-02-12 01:22:59 +0300

    final Task 6 Lesson 4 Level 7

--- src/Level7/Lesson4/Task5/Solution.java
+++ src/Level7/Lesson4/Task5/Solution.java
@@ -13,36 +13,38 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 //Требования:
-        //Программа должна создавать большой массив на 20 целых чисел.
-        //Программа должна считывать с клавиатуры 20 чисел для большого массива.
-        //Программа должна создавать два маленьких массива на 10 чисел каждый.
-        //Программа должна скопировать одну половину большого массива в первый маленький, а вторую - во второй и вывести второй маленький массив на экран.
+//Программа должна создавать большой массив на 20 целых чисел.
+//Программа должна считывать с клавиатуры 20 чисел для большого массива.
+//Программа должна создавать два маленьких массива на 10 чисел каждый.
+//Программа должна скопировать одну половину большого массива в первый маленький,
+// а вторую - во второй и вывести второй маленький массив на экран.
 public class Solution {
-            public static void main(String[] args) throws IOException {
-                BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-                int[] arrF = new int[20];
-                for (int i = 0; i < 20; i++) {
-                    arrF[i] = Integer.parseInt(reader.readLine());
-                }
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int[] arrF = new int[20];
+        for (int i = 0; i < 20; i++) {
+            arrF[i] = Integer.parseInt(reader.readLine());
+        }
 
-                int[] arrS = new int[10];
-                int[] arrT = new int[10];
+        int[] arrS = new int[10];
+        int[] arrT = new int[10];
 
-                for (int j = 0; j < 20; j++) {
-                    if (j==0 &&j < 10) {
-                        arrS[j] = arrF[j];
-                    } else if(j>=10&&j<20){
-                        arrT[j] = arrF[j];
-                    }
-                }
-                for (int g=0;g<10;g++){
-                    System.out.println(arrS[g]);
-                    System.out.println(arrT[g]);
-                }
+        for (int j = 0; j < 10; j++) {
+             arrS[j] = arrF[j];
+            }
+            for(int k=10;k<20;k++){
+                int arrIndex=k-10;
+                arrT[arrIndex] = arrF[k];
             }
 
+        for (int g = 0; g < 10; g++) {
+          System.out.println(arrT[g]);
+        }
+
+    }
+}
 
 
-            }
+
 
 

2d0e8deb6609c6dc521751fb8cea0d1df6bc3b27
Dmitry Kokhna <dmitry.kokhna@gmail.com>
2021-03-04 23:54:56 +0300

    final Task 6 Lesson 8 Level 8

--- src/Level8/Lesson8/Task6/Solution.java
+++ src/Level8/Lesson8/Task6/Solution.java
@@ -24,44 +24,54 @@ import java.util.Iterator;
 import java.util.Map;
 
 public class Solution {
+    public static void main(String[] args) {
+        removeItemFromMap(createMap());
 
-    public static Map<String, Integer> createMap(){
-        Map<String,Integer> staf = new HashMap<>();
-        staf.put("staf1",495);
-        staf.put("staf2",496);
-        staf.put("staf3",497);
-        staf.put("staf4",498);
-        staf.put("staf5",499);
-        staf.put("staf6",500);
-        staf.put("staf7",501);
-        staf.put("staf8",503);
-        staf.put("staf9",504);
-        staf.put("staf10",505);
+    }
+
+    public static Map<String, Integer> createMap() {
+        Map<String, Integer> staf = new HashMap<>();
+        staf.put("staf1", 495);
+        staf.put("staf2", 496);
+        staf.put("staf3", 497);
+        staf.put("staf4", 498);
+        staf.put("staf5", 499);
+        staf.put("staf6", 500);
+        staf.put("staf7", 501);
+        staf.put("staf8", 503);
+        staf.put("staf9", 504);
+        staf.put("staf10", 505);
 
         return staf;
     }
 
-    public static void removeItemFromMap(Map<String, Integer> map){
-
-        for(Map.Entry<String, Integer> pair : map.entrySet()){
-           if( pair.getValue()< 500)
-           {
-            map.remove(pair.getKey());
+    public static void removeItemFromMap(Map<String, Integer> map) {
+        Iterator<Map.Entry<String, Integer>> iter = map.entrySet().iterator();  // тут обязательно использовать итератор, т.к. удаление в переборе можно только в Итераторе
+        // А удаление в FOR/FOREACH нельзя делать!!!!!!!
+        while (iter.hasNext()) {
+            Map.Entry<String, Integer> pair = iter.next();
+            if (pair.getValue() < 500) {
+                iter.remove();
             }
-             }
-       System.out.println(map);
         }
+//        Iterator<Map.Entry<String, Integer>> iter2 = map.entrySet().iterator();  //-- это для проверки, что удаление работает
+//        while (iter2.hasNext()) {
+//            Map.Entry<String, Integer> pair2 = iter2.next();
+//            String key = pair2.getKey();
+//            Integer value = pair2.getValue();
+//            System.out.println(key+" "+value);
+//
+//
+//    }
 
 
 
 
-    public static void main(String[] args) {
-
- System.out.println(createMap().toString());
-
-       removeItemFromMap();
+        }
 
     }
 
 
-}
+
+
+

863bc10cb2a90a6830c1c2088c03c17ce88a47ab
Dmitry Kokhna <dmitry.kokhna@gmail.com>
2021-03-13 13:27:40 +0300

    final Task 4 Lesson 8 Level 8

--- src/Level8/Lesson8/Task4/Solution.java
+++ src/Level8/Lesson8/Task4/Solution.java
@@ -13,10 +13,7 @@ package Level8.Lesson8.Task4;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.Locale;
-import java.util.Map;
+import java.util.*;
 
 //Создать словарь (Map<String, Date>) и занести в него десять записей по принципу: "фамилия" - "дата рождения".
 //Удалить из словаря всех людей, родившихся летом.
@@ -32,28 +29,43 @@ public class Solution {
     public static Map<String, Date> createMap() throws ParseException {
         DateFormat dateFormat = new SimpleDateFormat("MMMM d yyyy", Locale.ENGLISH);
         Map<String, Date> map = new HashMap<>();
-        map.put("Смирнов",dateFormat.parse("MAY 1 2012"));
-        map.put("Смирнов2",dateFormat.parse("MAY 2 2012"));
-        map.put("Смирнов3",dateFormat.parse("MAY 3 2012"));
-        map.put("Смирнов4",dateFormat.parse("July 1 2012"));
-        map.put("Смирнов5",dateFormat.parse("July 2 2012"));
-        map.put("Смирнов6",dateFormat.parse("June 1 2012"));
-        map.put("Смирнов7",dateFormat.parse("July 2 2012"));
-        map.put("Смирнов8",dateFormat.parse("November 1 2012"));
-        map.put("Смирнов9",dateFormat.parse("November 9 2012"));
+        map.put("Весна 1",dateFormat.parse("MAY 1 2012"));
+        map.put("Весна 2",dateFormat.parse("MAY 2 2012"));
+        map.put("Весна 3",dateFormat.parse("MAY 3 2012"));
+        map.put("Лето 1 ",dateFormat.parse("July 1 2012"));
+        map.put("Лето 2",dateFormat.parse("July 2 2012"));
+        map.put("Лето 3",dateFormat.parse("June 1 2012"));
+        map.put("Лето 4",dateFormat.parse("July 2 2012"));
+        map.put("Осень 1",dateFormat.parse("September 1 2012"));
+        map.put("Осень 2",dateFormat.parse("November 9 2012"));
 
         return map;
     }
 
     public static void removeAllSummerPeople(Map<String ,Date> map) {
-        //код
+
+        Iterator<Map.Entry<String, Date>> iter = map.entrySet().iterator();
+        while (iter.hasNext()) {
+            Map.Entry<String, Date> pair = iter.next();
+            if (pair.getValue().getMonth() == Calendar.JUNE || pair.getValue().getMonth() == Calendar.JULY|| pair.getValue().getMonth() == Calendar.AUGUST) // минус 1, т.к. считается отНуля
+            {
+                iter.remove();
+            }
+
+        }
+   //     System.out.println(map);  // по заданию не требуется
     }
 
     public static void main(String[] args) throws ParseException {
-     //   System.out.println(createMap().toString());
-   //     for (Map.Entry<String,Date> map2 : createMap().entrySet()){
-  //          System.out.println(map2.getKey());
- //           System.out.println(map2.getValue());
-   //     }
+
+// Те требуется по Заданию
+//        for (Map.Entry<String,Date> map2 : createMap().entrySet()){
+//            System.out.println(map2.getKey());
+//            System.out.println(map2.getValue());
+//        }
+//        System.out.println(" _______________________________________");
+//
+//        removeAllSummerPeople(createMap());
     }
+
 }

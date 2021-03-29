8c36670e3f86109fb203cf2eea996df6f0afb914
Dmitry Kokhna <dmitry.kokhna@gmail.com>
2021-03-30 00:50:37 +0300

    in the pipeline Task 2 Lesson 11 Level 8

--- src/Level8/Lesson11/Task2/Solution.java
+++ src/Level8/Lesson11/Task2/Solution.java
@@ -11,12 +11,12 @@ package Level8.Lesson11.Task2;
 // И ещё — метод printPets, пусть выводит на экран всех обитателей pets
 
 
-//1. Внутри класса Solution создать public static классы Cat, Dog без конструктора или с конструктором без параметров.
-//2. Реализовать метод createCats, который должен возвращать множество с 4 котами.
-//3. Реализовать метод createDogs, который должен возвращать множество с 3 собаками.
-//4. Реализовать метод join, который должен возвращать объединенное множество всех животных - всех котов и собак.
+//1. + Внутри класса Solution создать public static классы Cat, Dog без конструктора или с конструктором без параметров.
+//2. + Реализовать метод createCats, который должен возвращать множество с 4 котами.
+//3. + Реализовать метод createDogs, который должен возвращать множество с 3 собаками.
+//4. + Реализовать метод join, который должен возвращать объединенное множество всех животных - всех котов и собак.
 //5. Реализовать метод removeCats, который должен удалять из множества pets всех котов, которые есть в множестве cats.
-//6. Реализовать метод printPets, который должен выводить на экран всех животных, которые в нем есть.
+//6. + Реализовать метод printPets, который должен выводить на экран всех животных, которые в нем есть.
 //Каждое животное с новой строки.
 //
 //Требования:
@@ -29,9 +29,19 @@ package Level8.Lesson11.Task2;
 //Метод printPets() должен выводить на экран всех животных, которые в нем есть. Каждое животное с новой строки.
 
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Set;
 
 public class Solution {
+    public static class Cat{
+        public Cat() {
+        }
+    }
+    public static class Dog{
+        public Dog() {
+        }
+    }
+
 
     public static void main(String[] args) {
         Set<Cat> cats=createCats();
@@ -40,30 +50,64 @@ public class Solution {
         Set<Object> pets = join(cats,dogs);
         printPets(pets);
 
+//        removeCats(pets,cats);
+//printPets(pets);
 
     }
 
     public static Set<Cat> createCats(){
         Set<Cat> result = new HashSet<Cat>();
+
+        Cat cat1=new Cat();
+        Cat cat2=new Cat();
+        Cat cat3=new Cat();
+        Cat cat4=new Cat();
+
+        result.add(cat1);
+        result.add(cat2);
+        result.add(cat3);
+        result.add(cat4);
+
         return result;
     }
 
     public static Set<Dog> createDogs(){
-      //code
-        return null;
+      Set<Dog> dogsSet = new HashSet<>();
+      Dog dog1 =  new Dog();
+        Dog dog2 =  new Dog();
+        Dog dog3 =  new Dog();
+
+        dogsSet.add(dog1);
+        dogsSet.add(dog2);
+        dogsSet.add(dog3);
+
+        return dogsSet;
     }
 
     public static Set<Object> join(Set<Cat> cats, Set<Dog> dogs){
-
-        return null;
+Set<Cat> catForJoin=createCats();
+Set<Dog> dogForJoin=createDogs();
+Set<Object> catsAndDogs = new HashSet<>(createCats());
+catsAndDogs.addAll(dogForJoin);
+        return catsAndDogs;
             }
 
             public static void removeCats(Set<Object> pets,Set<Cat> cats){
-        //code
+                Iterator<Object> objectIterator= pets.iterator();
+                Iterator<Cat> catIterator= cats.iterator();
+                while (objectIterator.hasNext()){
+                    boolean isCat=objectIterator.next().equals(catIterator.next());
+                    if(isCat==true){
+                        objectIterator.remove();
+                    }
+                }
             }
 
             public static void printPets(Set<Object> pets){
-        //code
+      Iterator<Object> objectIterator=pets.iterator();
+      while (objectIterator.hasNext()){
+          System.out.println(objectIterator.next());
+      }
             }
             //my code
 

04fb78ae2c109c52ae5949248ece579d95a19565
Dmitry Kokhna <dmitry.kokhna@gmail.com>
2021-04-04 17:33:49 +0300

    final Task 5 Lesson 11 Level 8

--- src/Level8/Lesson11/Task5/Solution.java
+++ src/Level8/Lesson11/Task5/Solution.java
@@ -44,21 +44,19 @@ public class Solution {
         String string = reader.readLine();
         System.out.println(toUpperCaseForFirstLetter(string));
     }
+
     private static String toUpperCaseForFirstLetter(String text) {
         StringBuilder builder = new StringBuilder(text);
         //выставляем первый символ заглавным, если это буква
-        if (Character.isAlphabetic(text.codePointAt(0)))
-        {
+        if (Character.isAlphabetic(text.codePointAt(0))) {
             builder.setCharAt(0, Character.toUpperCase(text.charAt(0)));
         }
 
         //крутимся в цикле, и меняем буквы, перед которыми пробел на заглавные
-        for (int i = 1; i < text.length(); i++)
-        {
-                     if (Character.isAlphabetic(text.charAt(i)) && Character.isSpaceChar(text.charAt(i - 1)))
-                {
-             builder.setCharAt(i, Character.toUpperCase(text.charAt(i)));
-         }
+        for (int i = 1; i < text.length(); i++) {
+            if (Character.isAlphabetic(text.charAt(i)) && Character.isSpaceChar(text.charAt(i - 1))) {
+                builder.setCharAt(i, Character.toUpperCase(text.charAt(i)));
+            }
         }
 
         return builder.toString();

31c3ded773754ed37a29cc1996c8dc78d9f62b27
Dmitry Kokhna <dmitry.kokhna@gmail.com>
2021-04-04 23:32:39 +0300

    add Task 9 Lesson 11 Level 8

--- src/Level8/Lesson11/Task9/Solution.java
+++ src/Level8/Lesson11/Task9/Solution.java
@@ -24,14 +24,41 @@ package Level8.Lesson11.Task9;
 //Метод isDateOdd() должен возвращать true, если количество дней с начала года - нечетное число, иначе false.
 //Метод main() должен вызывать метод isDateOdd().
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
+
 public class Solution {
 
-    public static void main(String[] args) {
-        System.out.println(isDateOdd("MAY 1 2013"));
+    public static void main(String[] args) throws ParseException {
+
+        System.out.println(isDateOdd("JANUARY 1 2000"));
+
     }
 
-    public static boolean isDateOdd(String date){
+    public static boolean isDateOdd(String date) throws ParseException {
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("MMMM d yyyy", Locale.ENGLISH);
+        Date dateInput = simpleDateFormat.parse(date);
 
-        return true;
+       // int year = dateInput.getYear();
+
+        Date dateStartYear = new Date();
+
+        dateStartYear.setDate(1);
+        dateStartYear.setMonth(Calendar.JANUARY);
+    //    dateStartYear.setYear(year);
+
+        long difDate = dateInput.getTime() - dateStartYear.getTime();
+
+        long day = difDate / (1000 * 3600 * 24);
+        if (day % 2 == 0) {
+            return false;
+        } else {
+            return true;
+        }
+
     }
 }
+

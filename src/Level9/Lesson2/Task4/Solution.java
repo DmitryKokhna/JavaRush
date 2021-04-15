47a1cfc6798224f9f529e98c129ac8ae80e2ba40
Dima <dmitry.kokhna@gmail.com>
2021-04-15 20:44:16 +0300

    add Task 4 Lesson 2 Level 9

---
+++ src/Level9/Lesson2/Task4/Solution.java
@@ -0,0 +1,79 @@
+package Level9.Lesson2.Task4;
+
+//Задача - 4
+
+//Стек-трейс длиной 10 вызовов
+
+//Мне повезло: в младших классах Марья Ивановна вечно писала на доске фразу:
+// «Функция, вызванная последней, должна завершиться самой первой».
+// И заставляла выучить её на память, заверяя, что когда-нибуть поймем.
+// Спасибо, Марья Ивановна, кажется, этот день настал.
+// Я даже могу написать код, чтобы получить стек-трейс длиной 10 вызовов. А вы можете?
+
+//Напиши код, чтобы получить стек-трейс длиной 10 вызовов. Метод main изменять нельзя.
+//
+//Требования:
+//В классе должно быть 10 методов (метод main не учитывать).
+//В переменной stackTraceLength должно получиться значение 10.
+//Каждый метод должен вызывать другой метод.
+//Используй метод Thread.currentThread().getStackTrace().
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        int stackTraceLength = method1().length - method10().length + 1;
+    }
+
+    public static StackTraceElement[] method1() {
+        return method2();
+    }
+
+    public static StackTraceElement[] method2() {
+        method3();
+        //напишите тут ваш код
+        return method3();
+    }
+
+    public static StackTraceElement[] method3() {
+        method4();
+        return method4();
+        //напишите тут ваш код
+    }
+
+    public static StackTraceElement[] method4() {
+        method5();
+        return method5();
+        //напишите тут ваш код
+    }
+
+    public static StackTraceElement[] method5() {
+        method6();
+        return method6();
+        //напишите тут ваш код
+    }
+
+    public static StackTraceElement[] method6() {
+        method7();
+        return method7();
+        //напишите тут ваш код
+    }
+
+    public static StackTraceElement[] method7() {
+        method8();
+        return method8();
+        //напишите тут ваш код
+    }
+
+    public static StackTraceElement[] method8() {
+        method9();
+        return method9();
+        //напишите тут ваш код
+    }
+
+    public static StackTraceElement[] method9() {
+        return method10();
+    }
+
+    public static StackTraceElement[] method10() {
+        return Thread.currentThread().getStackTrace();
+    }
+}
+

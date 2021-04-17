31016f5b69f0654a97f66cd0143ed06a32bb0aa1
Dmitry Kokhna <dmitry.kokhna@gmail.com>
2021-04-17 18:10:20 +0300

    final Task 4 Lesson 2 Level 9

--- src/Level9/Lesson2/Task4/Solution.java
+++ src/Level9/Lesson2/Task4/Solution.java
@@ -28,49 +28,41 @@ public class Solution {
     }
 
     public static StackTraceElement[] method2() {
-     //   method3();
-        //напишите тут ваш код
+           //напишите тут ваш код
        return method3();
     }
 
     public static StackTraceElement[] method3() {
-      //  method4();
-    return method4();
+         return method4();
         //напишите тут ваш код
     }
 
     public static StackTraceElement[] method4() {
-//        method5();
     return method5();
         //напишите тут ваш код
     }
 
     public static StackTraceElement[] method5() {
-//        method6();
      return method6();
         //напишите тут ваш код
     }
 
     public static StackTraceElement[] method6() {
-//        method7();
      return method7();
         //напишите тут ваш код
     }
 
     public static StackTraceElement[] method7() {
-//        method8();
     return method8();
         //напишите тут ваш код
     }
 
     public static StackTraceElement[] method8() {
-//        method9();
      return method9();
         //напишите тут ваш код
     }
 
     public static StackTraceElement[] method9() {
-
         return method10();
     }
 

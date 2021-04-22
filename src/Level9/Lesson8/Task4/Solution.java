48cacfe11c7e8fa03213604dbfafad333f62f826
Dmitry Kokhna <dmitry.kokhna@gmail.com>
2021-04-22 21:09:14 +0300

    add Task 4 Lesson 8 Level 9

---
+++ src/Level9/Lesson8/Task4/Solution.java
@@ -0,0 +1,48 @@
+package Level9.Lesson8.Task4;
+
+//Задача 4
+//Перехват checked-исключений
+//Уровень - EASY
+
+//На лекциях секретного центра JavaRush нас учили,
+// что если в методе выбрасываются (возникают) исключения ClassNotFoundException и FileNotFoundException,
+// программист просто обязан указать их в сигнатуре метода (в заголовке метода).
+// Это и есть checked исключения.
+// Давайте обработаем их в методе handleExceptions и выведем их на экран.
+
+//В методе handleExceptions обработайте все checked исключения.
+//Нужно вывести на экран возникшее checked исключение.
+//Можно использовать только один блок try..catch
+//
+//Требования:
+//Метод handleExceptions должен вызывать метод method1.
+//Метод handleExceptions должен вызывать метод method2.
+//Метод handleExceptions должен вызывать метод method3.
+//Метод handleExceptions должен использовать только один блок try..catch.
+//Метод handleExceptions должен отлавливать и выводить на экран все возникающие checked исключения.
+//Программа должна выводить на экран текст.
+
+import java.io.IOException;
+import java.rmi.RemoteException;
+
+public class Solution {
+    public static void main(String[] args) {
+        handleExceptions(new Solution());
+    }
+
+    public static void handleExceptions(Solution obj){
+      obj.method1();
+      obj.method2();
+      obj.method3();
+    }
+    public void method1() throws IOException{
+        throw  new IOException();
+    }
+    public  void method2() throws NoSuchFieldError{
+         throw new NoSuchFieldError();
+    }
+
+    public void method3() throws RemoteException{
+        throw new RemoteException();
+    }
+}

02b96728379672ffcfb2d53e119998e3d98af46d
Dmitry Kokhna <dmitry.kokhna@gmail.com>
2021-04-22 22:22:28 +0300

    add Task 6 Lesson 8 Level 9

---
+++ src/Level9/Lesson8/Task6/Solution.java
@@ -0,0 +1,42 @@
+package Level9.Lesson8.Task6;
+
+//Задача 6
+//Все свои, даже исключения
+//Уровень - EASY
+
+//Тема исключений в своё время будоражила умы молодых учёных и программистов,
+// а сегодня от неё вскипают мозги студентов JavaRush.
+// Попробуем создавать собственные исключения.
+// Для этого даны классы MyException, MyException2, MyException3, MyException4.
+// Унаследуем классы так, чтобы у вас появилось два любых checked- и два любых unchecked-исключения.
+
+
+//Есть четыре класса MyException, MyException2, MyException3, MyException4.
+//Унаследуй классы так, чтобы у тебя появилось любые два checked исключения и любые два unchecked исключения.
+//
+//Подсказка:
+//Изучи внимательно классы Exception1, Exception2, Exception3 из второй задачи этого блока.
+//
+//Требования:
+//В классе Solution должно быть 4 вложенных класса: MyException, MyException2, MyException3 и MyException4.
+//Все вложенные классы должны быть унаследованы от классов исключений.
+//Два класса из четырех должны быть checked исключениями.
+//Два класса из четырех должны быть unchecked исключениями.
+
+public class Solution {
+    public static void main(String[] args)  {
+
+        class MyException  {
+        }
+
+        class MyException2 {
+        }
+
+        class MyException3 {
+        }
+
+        class MyException4  {
+        }
+
+    }
+}

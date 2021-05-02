c47d18e9b9ddbec7495651f3f0a2522e1cd75a76
Dmitry Kokhna <dmitry.kokhna@gmail.com>
2021-05-02 13:52:56 +0300

    final Task 9 Lesson 11 Level 9

--- src/Level9/Lesson11/Task9/Solution.java
+++ src/Level9/Lesson11/Task9/Solution.java
@@ -24,24 +24,46 @@ package Level9.Lesson11.Task9;
 //Метод convertMapToSet должен создать и вернуть множество котов, полученных из переданного словаря.
 //Программа должна вывести всех котов из множества на экран.
 
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 public class Solution {
     public static void main(String[] args) {
-//        Map<String, Cat> map = createMap();
-//        Set<Cat> set = convertMapToSet(map);
-//        printCatSet(set);
+       Map<String, Cat> map = createMap();
+        Set<Cat> set = convertMapToSet(map);
+       printCatSet(set);
     }
 
-//    public static Map<String, Cat> createMap() {
-//
-//        //напишите тут ваш код
-//    }
+   public static Map<String, Cat> createMap() {
+       HashMap<String,Cat> catHashMap = new HashMap<>();
+       Cat cat1= new Cat("catName1");
+       Cat cat2= new Cat("catName2");
+       Cat cat3= new Cat("catName3");
+       Cat cat4= new Cat("catName4");
+       Cat cat5= new Cat("catName5");
+       Cat cat6= new Cat("catName6");
+       Cat cat7= new Cat("catName7");
+       Cat cat8= new Cat("catName8");
+       Cat cat9= new Cat("catName9");
+       Cat cat10= new Cat("catName10");
+       catHashMap.put("catNameMap1",cat1);
+       catHashMap.put("catNameMap2",cat2);
+       catHashMap.put("catNameMap3",cat3);
+       catHashMap.put("catNameMap4",cat4);
+       catHashMap.put("catNameMap5",cat5);
+       catHashMap.put("catNameMap6",cat6);
+       catHashMap.put("catNameMap7",cat7);
+       catHashMap.put("catNameMap8",cat8);
+       catHashMap.put("catNameMap9",cat9);
+       catHashMap.put("catNameMap10",cat10);
+return  catHashMap;
+      //напишите тут ваш код
+  }
 
-//    public static Set<Cat> convertMapToSet(Map<String, Cat> map) {
-//
-//    }
+    public static Set<Cat> convertMapToSet(Map<String, Cat> map) {
+Set<Cat> catSet = new HashSet<Cat>(map.values());  // тут если попытаться через  Set<Cat> catSet = map.values() - то получим ошибку по типу переменной.
+
+return catSet;
+   }
 
     public static void printCatSet(Set<Cat> set) {
         for (Cat cat : set) {

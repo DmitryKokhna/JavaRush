1bcba43b11e21d190e63abcdfffa59d6be0e2492
Dima <dmitry.kokhna@gmail.com>
2021-05-13 20:08:18 +0300

    add Task 10 Lesson 11 Level 10

--- src/Level10/Lesson11/Task7/Solution.java
+++ src/Level10/Lesson11/Task7/Solution.java
@@ -29,7 +29,7 @@ public class Solution {
         solution.A = 5;
         solution.B = 5 * B;
         solution.C = 5 * C * D;
-        Solution.D = 5 * D * C;
+         Solution.D = 5 * D * C;
 
         Solution.D = 5;
     }

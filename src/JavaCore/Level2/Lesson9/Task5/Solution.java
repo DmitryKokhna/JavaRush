e42b0c55ce3c89e9c928aca698ae00835dc21dfb
Dima <dmitry.kokhna@gmail.com>
2021-06-25 19:41:57 +0300

    Java CORE  Level 2  Lesson 9  ADD Task 5

---
+++ src/JavaCore/Level2/Lesson9/Task5/Solution.java
@@ -0,0 +1,27 @@
+package JavaCore.Level2.Lesson9.Task5;
+
+//Задача 5
+//Класс Human и интерфейсы CanRun
+
+//Уроень - EASY
+/*
+Напиши public класс Human (человек) и public интерфейсы CanRun (бежать/ездить), CanSwim(плавать).
+Добавь в каждый интерфейс по oдному методу.
+Добавь эти интерфейсы классу Human, но не реализуй методы.
+Объяви класс Human абстрактным.
+
+Требования:
+Класс Solution должен содержать интерфейс CanRun с одним методом.
+Класс Solution должен содержать интерфейс CanSwim с одним методом.
+Класс Solution должен содержать класс Human.
+Класс Human должен реализовывать интерфейсы CanRun и CanSwim.
+Класс Human должен быть абстрактным.
+У класса Human не должно быть методов.
+ */
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    //code
+}
